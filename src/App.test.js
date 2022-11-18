58626832f7988d4b78230e04c860e9233332502e
Kandukuru Udaya Sankar <udayasankar@Kandukurus-MacBook-Pro.local>
2022-11-18 11:59:52 +0530

    [Udaya/Sharmila] demo app testing gitleaks

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  const password = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY2ODc1Mjc5OCwiaWF0IjoxNjY4NzUyNzk4fQ.viVe6J3URVYfqh"
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
